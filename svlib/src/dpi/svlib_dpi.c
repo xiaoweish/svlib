55194e0cf98cc34b2755db0e6c75523b9317c09b
jonathan bromley <jonathan@verilab.com>
2014-03-17 22:14:14 +0000

    Fix Trac#17

--- svlib/src/dpi/svlib_dpi.c
+++ svlib/src/dpi/svlib_dpi.c
@@ -22,6 +22,12 @@
  *    limitations under the License.
 =============================================================================*/
 
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 500
+#endif /* __STDC_VERSION__ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
